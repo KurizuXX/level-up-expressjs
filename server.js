749773576da55caad87b1660cfc43baa20a51586
nxiiio <w.orellana@duocuc.cl>
2025-11-22 00:11:09 -0300

    fix - ahora si

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = require('./src/app')
 
 const PORT = process.env.PORT || 5000
 
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`${PORT}`)
-});
+module.exports = app;
